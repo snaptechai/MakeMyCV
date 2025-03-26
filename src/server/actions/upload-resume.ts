13b38c43d5666bc8cffd08e201941b9354ce6fe6
wovnep <122073572+wovnep@users.noreply.github.com>
2025-03-26 05:31:26 +0000

    fix change to openai file uploading

--- src/server/actions/upload-resume.ts
+++ src/server/actions/upload-resume.ts
@@ -1,13 +1,8 @@
 "use server";
-import axios from "axios";
-import fs from "fs";
-import moment from "moment";
-import mammoth from "mammoth";
-import { PdfReader } from "pdfreader";
-import { extname, join } from "path";
 import { getTemplatesWithParams } from "@/server/helpers/template";
-import { stat, mkdir, writeFile, unlink } from "fs/promises";
 import { getCareerLevel } from "@/server/actions/get-career-level";
+import { OpenAI } from "openai";
+import { extname } from "path";
 
 // Dates are not actually Date objects, but strings in the format "YYYY-MM-DD", make sure to parse them if you need to use them as Date objects.
 type ChatGptResponseContentType = {
@@ -46,75 +41,22 @@ type ChatGptResponseContentType = {
   }[];
 };
 
-type ChatGptResponseType = {
-  choices: {
-    index: number;
-    message: {
-      role: string;
-      content: string;
-      refusal: string | null;
-    };
-    logprobs: null;
-    finish_reason: string;
-  }[];
-};
+const openai = new OpenAI({
+  apiKey: process.env.CHATGPT_API_TOKEN,
+});
 
-export async function uploadResume(formData: FormData) {
-  const file = formData.get("resume") as File | null;
-  if (!file) {
-    throw new Error("Couldn't find the attached file.");
-  }
-
-  const fileType = file.type;
-  const fileExtension = extname(file.name).toLowerCase();
-  const supportedFileTypes = [
-    "application/pdf",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    "application/msword",
-  ];
-  const supportedFileExtensions = [".pdf", ".docx", ".doc"];
-
-  if (!supportedFileTypes.includes(fileType) && !supportedFileExtensions.includes(fileExtension)) {
-    throw new Error("Unsupported file type.");
-  }
-
-  const buffer = Buffer.from(await file.arrayBuffer());
-
-  const pathDist: string = join(process.cwd(), "/private/documents");
-  const relativeUploadDir = moment().format("YYYY-MM-DD-DD");
-  const uploadDir = join(pathDist, relativeUploadDir);
-
+async function askQuestionAboutFile(file: File, question: string) {
   try {
-    await stat(uploadDir);
-  } catch (e: any) {
-    if (e.code === "ENOENT") {
-      await mkdir(uploadDir, { recursive: true });
-    } else {
-      console.error("Error while trying to create directory when uploading a file\n", e);
-      throw new Error("Something went wrong.");
-    }
-  }
-  try {
-    const uniqueSuffix = `${Date.now()}_${Math.round(Math.random() * 1e9)}`;
-    const fileExtension = extname(file.name);
-    const originalFilename = file.name.replace(/\.[^/.]+$/, "");
-    const sanitizedFilename = originalFilename.replace(/[^a-zA-Z0-9_\u0600-\u06FF.]/g, "_");
-    const filename = `${sanitizedFilename}_${uniqueSuffix}${fileExtension}`;
-    await writeFile(`${uploadDir}/${filename}`, buffer);
+    const fileUpload = await openai.files.create({
+      file: file,
+      purpose: "assistants",
+    });
 
-    const finalFilePath = `${uploadDir}/${filename}`;
-    const fileType = file.type;
-
-    const resumeText = await parseResume(finalFilePath, fileType);
-
-    const chatGptResponse = await axios.post<ChatGptResponseType>(
-      "https://api.openai.com/v1/chat/completions",
-      {
-        model: "gpt-4o-mini",
-        messages: [
-          {
-            role: "system",
-            content: `Extract the following information from the resume text: Personal Information, Employment History, Education, Achievements, Certifications, Publications.
+    const assistant = await openai.beta.assistants.create({
+      name: "CV Assistant",
+      model: "gpt-4-1106-preview",
+      instructions: `
+                Extract the following information from the resume text: Personal Information, Employment History, Education, Achievements, Certifications, Publications.
                 The response should follow this format:
                 {
                 "is_valid_cv": "Boolean",
@@ -161,43 +103,96 @@ export async function uploadResume(formData: FormData) {
                 }
                 ]
                 }
-                If any section is not present, return an empty array for that section. If any field is not present or cannot be determined, return that field as null. And replace "N/A" with null. Set the description field to null if no clear description/responsibilities are provided in the resume. IF YOU THINK THIS IS NOT A VALID CV/RESUME, SET is_valid_cv TO false.`,
-          },
-          {
-            role: "user",
-            content: `Resume Text:\n${resumeText}\n\nJSON Format:\n{"is_valid_cv": "Boolean", "first_name": null, "last_name": null, "email": null, "mobile": null, "employment_history": [],"education": [],"achievements": [],"certifications": [],"publications": []}`,
-          },
-        ],
-        temperature: 0.8,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.CHATGPT_API_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      },
-    );
+                If any section is not present, return an empty array for that section. If any field is not present or cannot be determined, return that field as null. And replace "N/A" with null. Set the description field to null if no clear description/responsibilities are provided in the resume. IF YOU THINK THIS IS NOT A VALID CV/RESUME, SET is_valid_cv TO false.
+      `,
+      tools: [{ type: "file_search" }],
+    });
 
-    if (chatGptResponse.data.choices.length === 0) {
-      await unlink(finalFilePath);
-      throw new Error("Couldn't extract information from your cv/resume.");
+    const thread = await openai.beta.threads.create();
+
+    await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: question,
+      attachments: [
+        {
+          file_id: fileUpload.id,
+          tools: [{ type: "file_search" }],
+        },
+      ],
+    });
+
+    const run = await openai.beta.threads.runs.create(thread.id, {
+      assistant_id: assistant.id,
+    });
+
+    let completedRun;
+    while (true) {
+      completedRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+      if (completedRun.status === "completed") {
+        break;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
 
-    const extractedInformation = JSON.parse(
-      chatGptResponse.data.choices[0].message.content,
-    ) as ChatGptResponseContentType;
+    const messages = await openai.beta.threads.messages.list(thread.id);
+    const response = messages.data[0].content[0];
+
+    await openai.files.del(fileUpload.id);
+    await openai.beta.assistants.del(assistant.id);
+
+    if ("text" in response) {
+      return response.text;
+    }
+    return null;
+  } catch (error) {
+    console.error("Error processing file question:", error);
+    throw new Error("Failed to process file question");
+  }
+}
+
+export async function uploadResume(formData: FormData) {
+  const file = formData.get("resume") as File | null;
+
+  if (!file) {
+    throw new Error("Couldn't find the attached file.");
+  }
+
+  const fileType = file.type;
+  const fileExtension = extname(file.name).toLowerCase();
+  const supportedFileTypes = [
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/msword",
+  ];
+
+  const supportedFileExtensions = [".pdf", ".docx", ".doc"];
+
+  if (!supportedFileTypes.includes(fileType) && !supportedFileExtensions.includes(fileExtension)) {
+    throw new Error("Unsupported file type.");
+  }
+
+  const question = `JSON Format:\n{"is_valid_cv": "Boolean", "first_name": null, "last_name": null, "email": null, "mobile": null, "employment_history": [],"education": [],"achievements": [],"certifications": [],"publications": []}`;
+  const data = await askQuestionAboutFile(file, question);
+
+  if (!data) {
+    throw new Error("Couldn't extract information from your cv/resume.");
+  }
+
+  const jsonMatch = data.value.match(/\{[\s\S]*\}/);
+  if (!jsonMatch) {
+    throw new Error("Invalid response format from CV parsing.");
+  }
+
+  try {
+    const extractedInformation = JSON.parse(jsonMatch[0]) as ChatGptResponseContentType;
 
     if (!extractedInformation.is_valid_cv) {
-      await unlink(finalFilePath);
-
       throw new Error("Not A valid cv.");
     }
 
     const careerLevel = await getCareerLevel(extractedInformation);
 
     if (!careerLevel) {
-      await unlink(finalFilePath);
-
       throw new Error("Couldn't extract information from your cv/resume.");
     }
 
@@ -209,45 +204,14 @@ export async function uploadResume(formData: FormData) {
       number_of_publications: extractedInformation.publications.length,
     });
 
-    await unlink(finalFilePath);
-
     return {
       templates: templates,
       career_level: careerLevel.career_level,
       price: careerLevel.price,
       extracted_information: extractedInformation,
     };
-  } catch (e) {
-    console.error("Error while trying to upload a file\n", e);
-    throw new Error("Something went wrong.");
+  } catch (error) {
+    console.error("Error parsing CV data:", error);
+    throw new Error("Failed to parse CV information.");
   }
 }
-
-const parseResume = async (filePath: string, fileType: string | null) => {
-  if (fileType === "application/pdf") {
-    return new Promise<string>((resolve, reject) => {
-      const pdfReader = new PdfReader();
-      let pdfText = "";
-
-      pdfReader.parseFileItems(filePath, (err, item) => {
-        if (err) {
-          reject(err);
-        } else if (!item) {
-          resolve(pdfText);
-        } else if (item.text) {
-          pdfText += item.text + " ";
-        }
-      });
-    });
-  } else if (
-    fileType === "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
-    fileType === "application/msword" ||
-    fileType === "application/octet-stream"
-  ) {
-    const dataBuffer = fs.readFileSync(filePath);
-    const result = await mammoth.extractRawText({ buffer: dataBuffer });
-    return result.value;
-  } else {
-    throw new Error("Unsupported file type");
-  }
-};
